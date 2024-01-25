3dd02148c67b1fb3e0a076ed01dc6eebe9ed5a1f
Liam Forges <liamforges@Liams-MacBook-Pro.local>
2024-01-25 14:12:56 -0500

    action heuristic

--- src/funkyguy3/MainPhase.java
+++ src/funkyguy3/MainPhase.java
@@ -1,6 +1,7 @@
 package funkyguy3;
 
 import battlecode.common.*;
+import battlecode.world.Trap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,11 +18,31 @@ public class MainPhase {
     public static RobotInfo[] nearbyAllies = null;
     public static RobotInfo[] nearbyEnemies = null;
     public static MapLocation[] broadcastFlags = null;
+    public static int crumbs = 0;
+    public static int TRAP_COOLDOWN = 5;
+    public static int BOMB_COST = 200;
+    public static int STUN_COST = 100;
+    public static int VISION_AREA = 60;
+    public static int BOMB_AREA = 14;
+    public static int STUN_AREA = 32;
+    public static int BOMB_DMG = 750;
+    public static int HEAL_AMT = 80;
+
+    public static int ATTACK_AMT = 150;
+    public static float STUN_ATTACK_CHANCE = 0.3f;
+
+    public static String[] actions = new String[]{"heal", "attack", "stun", "bomb", "none"};
 
     public static void runMainPhase(RobotController rc) throws GameActionException {
         // ACTIONS
-        if (rc.canBuyGlobal(GlobalUpgrade.ACTION)) rc.buyGlobal(GlobalUpgrade.ACTION);
-        if (rc.canBuyGlobal(GlobalUpgrade.HEALING)) rc.buyGlobal(GlobalUpgrade.HEALING);
+        if (rc.canBuyGlobal(GlobalUpgrade.ACTION)) {
+            rc.buyGlobal(GlobalUpgrade.ACTION);
+            ATTACK_AMT += 60;
+        }
+        if (rc.canBuyGlobal(GlobalUpgrade.HEALING)) {
+            rc.buyGlobal(GlobalUpgrade.HEALING);
+            HEAL_AMT += 50;
+        }
         if (rc.canBuyGlobal(GlobalUpgrade.CAPTURING)) rc.buyGlobal(GlobalUpgrade.CAPTURING);
 
 
@@ -36,12 +57,10 @@ public class MainPhase {
             nearbyAllies = rc.senseNearbyRobots(-1, rc.getTeam());
             nearbyEnemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
             broadcastFlags = rc.senseBroadcastFlagLocations();
+            crumbs = rc.getCrumbs();
+            nextAction(rc);
             nextMove(rc);
-//            attackEnemies(rc);
-//
-//            healAllies(rc);
-//
-//            captureTheFlag(rc);
+//            nextAction(rc);
         }
         else {
             // only tries to add more traps if nearby bread
@@ -100,49 +119,15 @@ public class MainPhase {
         RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
         for (RobotInfo robot: nearbyEnemies) {
             if (robot.hasFlag() && !rc.hasFlag()) {
-                Pathfind.moveTowards(rc, robot.getLocation());
                 if (rc.canAttack(robot.getLocation())) rc.attack(robot.getLocation());
             }
         }
         for (RobotInfo robot: nearbyEnemies) {
-            if (!rc.hasFlag()) {
-                Pathfind.moveTowards(rc, robot.getLocation());
-            }
             if (rc.canAttack(robot.getLocation()) && !rc.hasFlag()) {
                 rc.attack(robot.getLocation());
             }
         }
-        if (!rc.hasFlag()){
-            FlagInfo[] nearbyFlags = rc.senseNearbyFlags(-1, rc.getTeam().opponent());
-            for (FlagInfo flag : nearbyFlags) {
-                if (flag.isPickedUp()) {
-                    // build occasional defensive traps near flags
-                    if (RobotPlayer.rng.nextInt(25) == 0){
-                        if (rc.canBuild(TrapType.STUN, rc.getLocation())){
-                            rc.build(TrapType.STUN, rc.getLocation());
-                        }
-                    }
-                }
-                else {
-                    MapLocation[] allySpawns = rc.getAllySpawnLocations();
-                    Direction dir = rc.getLocation().directionTo(allySpawns[RobotPlayer.rng.nextInt(allySpawns.length)]);
-                    if (rc.canBuild(TrapType.STUN, rc.getLocation().add(dir))) rc.build(TrapType.STUN, rc.getLocation().add(dir));
-                }
-            }
-            if (nearbyEnemies.length >= BOMBING_THRESHOLD){
-                if (rc.canBuild(TrapType.EXPLOSIVE, rc.getLocation())) {
-                    System.out.println("BOMBING");
-                    rc.build(TrapType.EXPLOSIVE, rc.getLocation());
-                }
-            }
-            else if (nearbyEnemies.length >= STUNNING_THRESHOLD) {
-                if (rc.canBuild(TrapType.STUN, rc.getLocation())) {
-                    System.out.println("STUNNING");
-                    rc.build(TrapType.STUN, rc.getLocation());
-                }
-            }
 
-        }
     }
 
     private static void captureTheFlag(RobotController rc) throws GameActionException {
@@ -199,6 +184,7 @@ public class MainPhase {
         float cNearbyAllyFlags = -10;
         float cNearbyEnemyFlags = -100;
         float cBroadcastFlags = -10;
+        float cFlagHolder = -100;
         MapLocation center = rc.getLocation().add(direction);
         if (!isLocOnMap(rc, center) || rc.senseMapInfo(center).isWall()) {
             return -INF;
@@ -223,15 +209,23 @@ public class MainPhase {
                 res += cEnemy*center.distanceSquaredTo(enemy.getLocation());
             }
         }
-        for (FlagInfo flag : nearbyEnemyFlags) {
-            res += cNearbyEnemyFlags*center.distanceSquaredTo(flag.getLocation());
+        if (rc.hasFlag()) {
+            MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+            res += cFlagHolder*center.distanceSquaredTo(Pathfind.findClosestLocation(center, Arrays.asList(spawnLocs)));
         }
-        for (FlagInfo flag : nearbyAllyFlags) {
-            res += cNearbyAllyFlags*center.distanceSquaredTo(flag.getLocation());
+        else {
+            for (FlagInfo flag : nearbyEnemyFlags) {
+                res += cNearbyEnemyFlags*center.distanceSquaredTo(flag.getLocation());
+            }
+            if (nearbyEnemyFlags.length == 0) {
+                for (MapLocation flagLoc : broadcastFlags) {
+                    res += cBroadcastFlags*center.distanceSquaredTo(flagLoc);
+                }
+            }
         }
-        for (MapLocation flagLoc : broadcastFlags) {
-            res += cBroadcastFlags*center.distanceSquaredTo(flagLoc);
-        }
+//        for (FlagInfo flag : nearbyAllyFlags) {
+//            res += cNearbyAllyFlags*center.distanceSquaredTo(flag.getLocation());
+//        }
 
         return res;
     }
@@ -241,7 +235,7 @@ public class MainPhase {
         Direction maxDirection = null;
         for (Direction dir : RobotPlayer.directions) {
             float newHeuristic = moveHeuristic(rc, dir);
-            if (Float.compare(newHeuristic,maxHeuristic) > 0) {
+            if (Float.compare(newHeuristic, maxHeuristic) > 0) {
                 maxDirection = dir;
                 maxHeuristic = newHeuristic;
             }
@@ -251,7 +245,84 @@ public class MainPhase {
         }
     }
 
+    private static float actionHeuristic(RobotController rc, String action) {
+        float res = 0;
+        float cCooldown = 1;
+        float cCrumbs = 1;
+        float cEnemyHealth = -5;
+        float cAllyHealth = 10;
+        float allyHealth = 0;
+        float enemyHealth = 0;
+        int hCrumbs = crumbs;
+        for (RobotInfo ally : nearbyAllies) {
+            allyHealth += ally.getHealth();
+        }
+        for (RobotInfo enemy : nearbyEnemies) {
+            enemyHealth += enemy.getHealth();
+        }
+        switch (action) {
+            case "heal":
+                res += cCooldown*(rc.getActionCooldownTurns() + GameConstants.HEAL_COOLDOWN - GameConstants.COOLDOWNS_PER_TURN);
+                allyHealth += HEAL_AMT;
+            case "attack":
+                res += cCooldown*(rc.getActionCooldownTurns() + GameConstants.ATTACK_COOLDOWN - GameConstants.COOLDOWNS_PER_TURN);
+                enemyHealth -= ATTACK_AMT;
+            case "stun":
+                res += cCooldown*(rc.getActionCooldownTurns() + TRAP_COOLDOWN - GameConstants.COOLDOWNS_PER_TURN);
+                hCrumbs -= STUN_COST;
+                // ~%chance of activation * estimated number of enemies stunned * x% chance of getting attacked while stunned * attack damage;
+                enemyHealth -= (float) (nearbyEnemies.length/VISION_AREA)*((float) (nearbyEnemies.length/VISION_AREA)*STUN_AREA) * STUN_ATTACK_CHANCE * ATTACK_AMT;
+            case "bomb":
+                res += cCooldown*(rc.getActionCooldownTurns() + TRAP_COOLDOWN - GameConstants.COOLDOWNS_PER_TURN);
+                // %chance of activation * estimated # of enemies it will hit * damage
+                enemyHealth -= (float) (nearbyEnemies.length/VISION_AREA)*((float) (nearbyEnemies.length/VISION_AREA)*BOMB_AREA)*BOMB_DMG;
+                hCrumbs -= BOMB_COST;
+            case "none":
+                res += cCooldown*(rc.getActionCooldownTurns() - GameConstants.COOLDOWNS_PER_TURN);
+        }
+        res += cCrumbs*hCrumbs + cEnemyHealth*enemyHealth + cAllyHealth*allyHealth;
+        return res;
+    }
+
+    private static void nextAction(RobotController rc) throws GameActionException{
+        float maxHeuristic = -INF;
+        String bestAction = "";
+        for (String action : actions) {
+            float newHeuristic = actionHeuristic(rc, action);
+            if (Float.compare(newHeuristic, maxHeuristic) > 0) {
+                maxHeuristic = newHeuristic;
+                bestAction = action;
+            }
+        }
+        switch (bestAction) {
+            case "attack":
+                attackEnemies(rc);
+                rc.setIndicatorString("Got 'eem");
+            case "heal":
+                healAllies(rc);
+                rc.setIndicatorString("Healing eh");
+            case "bomb":
+                if (rc.canBuild(TrapType.EXPLOSIVE, rc.getLocation())) {
+                    rc.build(TrapType.EXPLOSIVE, rc.getLocation());
+                }
+                rc.setIndicatorString("GET DOWN!!");
+            case "stun":
+                if (rc.canBuild(TrapType.STUN, rc.getLocation())) {
+                    rc.build(TrapType.STUN, rc.getLocation());
+                }
+                rc.setIndicatorString("Yung stunner");
+            case "flag":
+                getFlag(rc);
+            case "none":
+                assert true;
+                rc.setIndicatorString("Vibin'");
+        }
+    }
     private static Boolean isLocOnMap(RobotController rc, MapLocation loc) throws GameActionException{
         return loc.x >= 0 && loc.y >= 0 && loc.x < rc.getMapWidth() && loc.y < rc.getMapHeight();
+    }
+
+    private static void getFlag(RobotController rc) throws GameActionException {
+
     }
 }
